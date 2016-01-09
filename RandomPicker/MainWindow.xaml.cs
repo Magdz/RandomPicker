f87905a84eb430a48caeba47ac5ebcdb96b5c0b1
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-01-09 03:55:31 +0200

    initiate Load

--- RandomPicker/MainWindow.xaml.cs
+++ RandomPicker/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RandomPicker
 {
@@ -29,6 +18,12 @@ namespace RandomPicker
         {
             Manual manual = new Manual();
             manual.Show();
+        }
+
+        private void Load_Clicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
         }
 
         private void Pick_Button_Click(object sender, RoutedEventArgs e)
